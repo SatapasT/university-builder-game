320da3a63f152a25596251a76f0ae1723c18da94
SatapasT <satapastanachotpaisit@gmail.com>
2025-12-08 04:40:33 +0000

    added camera movement

---
+++ University Builder/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivity = 20f;
+
+    public Transform orientation;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Mouse.current == null) {
+            return;
+        }
+
+        Vector2 delta = Mouse.current.delta.ReadValue();
+        float mouseX = delta.x * sensitivity * Time.deltaTime;
+        float mouseY = delta.y * sensitivity * Time.deltaTime;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+}
